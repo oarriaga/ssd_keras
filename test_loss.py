9f3c892645830e9798e8ad441bfe43aa8d300553
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-03-20 23:22:59 +0100

    Added multibox loss test

--- test_loss.py
+++ test_loss.py
@@ -6,8 +6,7 @@
 #import cv2
 import keras
 from keras.applications.imagenet_utils import preprocess_input
-from keras.preprocessing import image
-import matplotlib.pyplot as plt
+#from keras.preprocessing import image
 import numpy as np
 import pickle
 from random import shuffle
@@ -15,9 +14,11 @@ from scipy.misc import imread
 from scipy.misc import imresize
 #import tensorflow as tf
 from PASCAL_VOC.get_data_from_XML import XML_preprocessor
+from image_generator import ImageGenerator
 
 from ssd import SSD300
-from ssd_training import MultiboxLoss
+#from ssd_training import MultiboxLoss
+from multibox_loss import MultiboxLoss
 from ssd_utils import BBoxUtility
 
 
@@ -227,10 +228,12 @@ class Generator(object):
 path_prefix = 'VOC2007/JPEGImages/'
 #path_prefix = '../../frames/'
 # BATCH 11 is the maximum 
-gen = Generator(gt, bbox_util, 9, path_prefix,
-                train_keys, val_keys,
-                (input_shape[0], input_shape[1]), do_crop=False)
-
+#gen = Generator(gt, bbox_util, 11, path_prefix,
+#                train_keys, val_keys,
+#                (input_shape[0], input_shape[1]), do_crop=False)
+#
+gen = ImageGenerator(gt, bbox_util, 9, (input_shape[0], input_shape[1]),
+                train_keys, val_keys, path_prefix)
 # In[7]:
 
 model = SSD300(input_shape, num_classes=NUM_CLASSES)
@@ -271,72 +274,22 @@ model.compile(optimizer=optim,
 
 
 # In[11]:
+#
+#nb_epoch = 30
+#history = model.fit_generator(gen.generate(True), gen.train_batches,
+#                              nb_epoch, verbose=1,
+#                              callbacks=callbacks,
+#                              validation_data=gen.generate(False),
+#                              nb_val_samples=gen.val_batches,
+#                              nb_worker=1)
+#
 
 nb_epoch = 30
-history = model.fit_generator(gen.generate(True), gen.train_batches,
+history = model.fit_generator(gen.flow(mode='train'), len(train_keys),
                               nb_epoch, verbose=1,
                               callbacks=callbacks,
-                              validation_data=gen.generate(False),
-                              nb_val_samples=gen.val_batches,
+                              validation_data=gen.flow('val'),
+                              nb_val_samples=len(val_keys),
                               nb_worker=1)
 
 
-# In[12]:
-
-inputs = []
-images = []
-img_path = path_prefix + sorted(val_keys)[0]
-img = image.load_img(img_path, target_size=(300, 300))
-img = image.img_to_array(img)
-images.append(imread(img_path))
-inputs.append(img.copy())
-inputs = preprocess_input(np.array(inputs))
-
-
-# In[13]:
-
-preds = model.predict(inputs, batch_size=1, verbose=1)
-results = bbox_util.detection_out(preds)
-
-
-# In[14]:
-
-for i, img in enumerate(images):
-    # Parse the outputs.
-    det_label = results[i][:, 0]
-    det_conf = results[i][:, 1]
-    det_xmin = results[i][:, 2]
-    det_ymin = results[i][:, 3]
-    det_xmax = results[i][:, 4]
-    det_ymax = results[i][:, 5]
-
-    # Get detections with confidence higher than 0.6.
-    top_indices = [i for i, conf in enumerate(det_conf) if conf >= 0.6]
-
-    top_conf = det_conf[top_indices]
-    top_label_indices = det_label[top_indices].tolist()
-    top_xmin = det_xmin[top_indices]
-    top_ymin = det_ymin[top_indices]
-    top_xmax = det_xmax[top_indices]
-    top_ymax = det_ymax[top_indices]
-
-    colors = plt.cm.hsv(np.linspace(0, 1, 4)).tolist()
-
-    plt.imshow(img / 255.)
-    currentAxis = plt.gca()
-
-    for i in range(top_conf.shape[0]):
-        xmin = int(round(top_xmin[i] * img.shape[1]))
-        ymin = int(round(top_ymin[i] * img.shape[0]))
-        xmax = int(round(top_xmax[i] * img.shape[1]))
-        ymax = int(round(top_ymax[i] * img.shape[0]))
-        score = top_conf[i]
-        label = int(top_label_indices[i])
-#         label_name = voc_classes[label - 1]
-        display_txt = '{:0.2f}, {}'.format(score, label)
-        coords = (xmin, ymin), xmax-xmin+1, ymax-ymin+1
-        color = colors[label]
-        currentAxis.add_patch(plt.Rectangle(*coords, fill=False, edgecolor=color, linewidth=2))
-        currentAxis.text(xmin, ymin, display_txt, bbox={'facecolor':color, 'alpha':0.5})
-    
-    plt.show()
